53572c683e1bfb9b5f1fd24ee2e6eb483814e366
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-21 10:17:45 -0300

    Consumindo o Endpoint de Cambio Service no Book Service

--- src/main/java/br/com/moser/controller/BookController.java
+++ src/main/java/br/com/moser/controller/BookController.java
@@ -1,6 +1,7 @@
 package br.com.moser.controller;
 
 import br.com.moser.model.Book;
+import br.com.moser.response.Cambio;
 import br.com.moser.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 /**
  * @author Juliano Moser
@@ -26,8 +30,21 @@ public class BookController {
                          @PathVariable("currency") String currency) {
 
         Book book = bookService.findById(id);
+
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                        Cambio.class, params);
+
+        var cambio = response.getBody();
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
