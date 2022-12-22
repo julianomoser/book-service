d67f10e6c09a674dd02818d4e646f344ba647998
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-22 03:46:50 -0300

    Features de Rate Limit e BulkHead no Resilience4j

--- src/main/java/br/com/moser/controller/FooBarController.java
+++ src/main/java/br/com/moser/controller/FooBarController.java
@@ -1,12 +1,10 @@
 package br.com.moser.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Juliano Moser
@@ -19,12 +17,15 @@ public class FooBarController {
 
     @GetMapping("/foo-bar")
 //    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar() {
         log.info("Request to Foo-Bar is Received!");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "Foo-Bar!!";
     }
 
     public String fallbackMethod(Exception ex) {
